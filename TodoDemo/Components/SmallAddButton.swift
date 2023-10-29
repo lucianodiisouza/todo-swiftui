2f7c19528086c63117a5dc1ef1fe5919458d0146
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-10-29 14:37:55 -0300

    adding AddTaskView

---
+++ TodoDemo/Components/SmallAddButton.swift
@@ -0,0 +1,18 @@
+//
+//  SmallAddButton.swift
+//  TodoDemo
+//
+//  Created by Luciano dii Souza on 29/10/23.
+//
+
+import SwiftUI
+
+struct SmallAddButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SmallAddButton()
+}
---
+++ TodoDemo/Views/AddTaskView.swift
@@ -0,0 +1,44 @@
+//
+//  AddTaskView.swift
+//  TodoDemo
+//
+//  Created by Luciano dii Souza on 29/10/23.
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    @State private var title: String = ""
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Create a new task")
+                .font(.title3).bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            TextField("Enter you task here", text: $title)
+                .textFieldStyle(.roundedBorder)
+            
+            Button {
+                print("Task added!")
+            } label: {
+                Text("Add task")
+                    .foregroundColor(.white)
+                    .padding()
+                    .padding(.horizontal)
+                    .background(Color(hue: 0.328, saturation: 0.796, brightness: 0.488))
+                    .cornerRadius(30)
+            }
+            
+            Spacer()
+            
+        }
+        .padding(.top, 40)
+        .padding(.horizontal)
+        .background(Color(hue: 0.086, saturation: 0.141, brightness: 0.972))
+    }
+}
+
+#Preview {
+    AddTaskView()
+}
