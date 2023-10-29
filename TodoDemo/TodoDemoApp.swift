e8d9a04dd9ead19634917765bd6f286322e15860
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-10-29 10:52:01 -0300

    Initial Commit

---
+++ TodoDemo/TodoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoDemoApp.swift
+//  TodoDemo
+//
+//  Created by Luciano dii Souza on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
