e87abda8fdb3b1657658bb309f73a31081364306
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-10-29 19:34:51 -0300

    feat: update action

--- TodoDemo/RealmManager.swift
+++ TodoDemo/RealmManager.swift
@@ -54,5 +54,22 @@ class RealmManager: ObservableObject {
         }
     }
     
-    
+    func updateTask(id: ObjectId, completed: Bool) {
+        if let localRealm = localRealm {
+            do {
+                let taskToUpdate = localRealm.objects(Task.self).filter(NSPredicate(format: "id == %@", id))
+                guard !taskToUpdate.isEmpty else { return }
+                
+                try localRealm.write {
+                    taskToUpdate[0].completed = completed
+                    getTasks()
+                    print("Updated task with id \(id)! Completed status \(completed)")
+                }
+                
+            } catch {
+                print("Error updating task \(id) to Realm: \(error)")
+            }
+        }
+        
+    }
 }
