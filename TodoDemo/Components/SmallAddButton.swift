45f1548ed51af56350d5c084360deaafd191cc0a
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-10-29 14:44:46 -0300

    add SmallAddbutton

--- TodoDemo/Components/SmallAddButton.swift
+++ TodoDemo/Components/SmallAddButton.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct SmallAddButton: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Circle()
+                .frame(width: 50)
+                .foregroundColor(Color(hue: 0.328, saturation: 0.796, brightness: 0.488))
+            
+            Text("+").font(.title)
+                .fontWeight(.heavy)
+                .foregroundColor(.white)
+            
+        }
+        .frame(height: 50)
     }
 }
 
